9bbd4ee3fb19031c9feffb03a0be68955adcfac1
Cha1552164 <chamithgithub>
2023-01-14 08:53:18 +0530

    Spring bean One instantiate

---
+++ SpringFramework/02BeanScopes/src/lk/ijse/spring/pojo/SpringBeanOne.java
@@ -0,0 +1,12 @@
+package lk.ijse.spring.pojo;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class SpringBeanOne {
+    public SpringBeanOne(){
+        System.out.println("Spring one instantiate");
+
+    }
+
+}
