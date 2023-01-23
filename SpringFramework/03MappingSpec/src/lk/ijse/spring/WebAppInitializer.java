ce854e3e959f28e8ba302367f4f046a89cd4b81b
Cha1552164 <chamithgithub>
2023-01-23 13:12:29 +0530

    method implement

--- SpringFramework/03MappingSpec/src/lk/ijse/spring/WebAppInitializer.java
+++ SpringFramework/03MappingSpec/src/lk/ijse/spring/WebAppInitializer.java
@@ -1,4 +1,20 @@
 package lk.ijse.spring;
 
-public class WebAppInitializer {
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class WebAppInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return new Class[]{};
+    }
+
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class[0];
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+        return new String[0];
+    }
 }
