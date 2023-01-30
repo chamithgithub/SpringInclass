5f9a33d51c31c7b15589b269409d5a945e6cb97a
Cha1552164 <chamithgithub>
2023-01-30 22:41:52 +0530

    create some clases

---
+++ SpringFramework/SpringWithMaven/src/main/java/lk/ijse/spring/config/WebAppConfig.java
@@ -0,0 +1,12 @@
+package lk.ijse.spring.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@ComponentScan(basePackages = "lk.ijse.spring.controller")
+@Configuration
+@EnableWebMvc
+
+public class WebAppConfig {
+}
